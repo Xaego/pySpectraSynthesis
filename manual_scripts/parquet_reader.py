acd0f62ddcd8fd3c968f2d2979e0ce432be27fb9
xaego <xaego@gmail.com>
2025-03-29 08:20:35 +0100

    tested ods vs xlsx

--- manual_scripts/parquet_reader.py
+++ manual_scripts/parquet_reader.py
@@ -1,4 +1,31 @@
-import pandas as pd
+# 83 sec for ods, probably due archive structure of ods format
+# 7 sec xlsx
 
-df = pd.read_parquet(r"/data/light_sources.parquet")
-print(df.head())
+import pandas as pd
+from loguru import logger
+from pathlib import Path
+import time
+
+start = time.time()
+
+# Определяем корневую папку проекта
+project_root = Path(__file__).resolve().parents[1]
+logger.debug(f"project_root: {project_root}")
+
+# Пути к файлам
+parquet_path = project_root / "data" / "light_sources.parquet"
+ods_path = project_root / "data" / "light_sources.ods"
+xlsx_path = project_root / "data" / "light_sources.xlsx"
+logger.debug(f"ods_path: {parquet_path}")
+logger.debug(f"xlsx_path: {xlsx_path}")
+
+# Чтение Parquet
+df = pd.read_parquet(parquet_path)
+logger.debug(df)
+
+# Сохранение в ODS
+# df.to_excel(ods_path, engine="odf", index=False)
+df.to_excel(xlsx_path, index=False)
+
+end = time.time()
+logger.success(f"Done in {end - start:.2f} seconds")
